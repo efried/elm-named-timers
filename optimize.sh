512171e3224aab8cd6a1c730a9750192f401a9bb
Evan Friedenberg <evan.friedenberg@gmail.com>
2022-12-15 09:06:37 -0500

    chore: update yarn, update script;

--- optimize.sh
+++ optimize.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-js="dist/elm.js"
+js="elm.js"
 min="dist/elm.min.js"
 
 npx --no-install elm make --optimize --output=$js "$@"
 
-npx --no-install uglify-js $js --compress 'pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe' | npx uglify-js --mangle --output $min
+npx --no-install uglify-js $js --compress 'pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe' | npx --no-install uglify-js --mangle --output $min
 
 echo "Compiled size:$(wc $js -c) bytes  ($js)"
 echo "Minified size:$(wc $min -c) bytes  ($min)"
